0df595b974b4dd4e50764845e368f7811fde4ebd
Rafael Grande <rafael.grande@jussi.com.br>
2021-04-15 04:57:57 -0300

    Change import component link

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import Button from '../../components/Input/Button'
+import Button from '../../components/Button'
 import Input from '../../components/Input'
 import Label from '../../components/Label'
 
