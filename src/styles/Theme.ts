e140aa69ed7002561c80cc2190fb0363be49ad55
Rafael Grande <rafael.grande@jussi.com.br>
2021-04-15 04:54:37 -0300

    Restore Theme file.

---
+++ src/styles/Theme.ts
@@ -0,0 +1,12 @@
+export default {
+    colors: {
+        primary: '#DCDCDC',
+        secondary: '#FFF',
+        tertiary: '#f2f2f2',
+        black: '#333',
+        gray: '#BFBFBF',
+        purple: '#b721ff',
+        warning: '#E44C4E'
+
+    }
+} 
