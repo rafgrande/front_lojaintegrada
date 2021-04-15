0f0447a3aa18921d7c5d01239d8dd24b867c7ccc
Rafael Grande <rafael.grande@jussi.com.br>
2021-04-15 04:34:05 -0300

    Change repository name

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import Button from '../../components/Button'
-import Input from '../../components/Input'
+import Input from '../../Components/Input'
 import Label from '../../components/Label'
 
 import logo from '../../assets/logo.png'
